57ddb010b20f5942fb75d6555451267925ae5db9
Your Name <you@example.com>
2017-02-23 21:47:09 +0530

    abc

---
+++ week1/addn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int add(int n)
+{
+return 0;
+}
+int main()
+{
+int i,n;
+int sum=0;
+int a[100];
+printf("enter the number of integers u want u add\n");
+scanf("%d",&n);
+printf("sum=%d",add(n));
+}
+
