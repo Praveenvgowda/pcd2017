94f3685dc4281b973e3f606f70e746346e394b37
Your Name <you@example.com>
2017-02-26 13:54:41 +0530

    z

--- week1/sumn.c
+++ week1/sumn.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
+
 int i,n;
 int sum=0;
 int a[100];
 printf("enter the number of integers you want to add\n");
 scanf("%d",&n);
 printf("enter %d integers\n",n);
-for(i=1;i<=n;i++);
+for(i=1;i<n;i++);
 {
 scanf("%d",&a[i]);
 sum=sum+a[i];
